7f9e9c7bd77019d78ca3c9e39d770fd5b6f846be
HaroldoTeruya <Haroldo_Teruya@hotmail.com>
2018-04-17 12:11:38 -0300

    android changed completely in this point. Need to remove all about socketio. The responsability to wakeup the application is to OneSignal. Documentation is ready in the wiki page. Ready to push to master. In the android, do not really exist a library, just to copy and paste an class inside the OneSignal node_modules package and add lines of code in the MainActivity

--- module/call/index.android.js
+++ module/call/index.android.js
@@ -1,69 +1,65 @@
 /**
  * @author Haroldo Shigueaki Teruya <haroldo.s.teruya@gmail.com>
- * @version 1.0.0
+ * @version 1.1.0
  */
 
-//==========================================================================
-// IMPORTS
+ //==========================================================================
+ // IMPORTS
 
-/**
- * This class requires:
- * @class
- * @requires [BaseCallManager]{@link ./base/BaseCallManager}
- * @requires DeviceEventEmitter from react-native
- * @requires NativeModules from react-native
- */
-import BaseCallManager from './base/BaseCallManager';
-import { DeviceEventEmitter, NativeModules } from 'react-native';
+ /**
+  * This class requires:
+  * @class
+  * @requires [BaseCallManager]{@link ./base/BaseCallManager}
+  * @requires DeviceEventEmitter from react-native
+  * @requires NativeModules from react-native
+  */
+ import BaseCallManager from './base/BaseCallManager';
+ import { DeviceEventEmitter, NativeModules } from 'react-native';
 
-//==========================================================================
+ //==========================================================================
 
-/**
- * @class
- * @classdesc
- */
-class CallManager extends BaseCallManager {
+ /**
+  * @class
+  * @classdesc
+  */
+ class CallManager extends BaseCallManager {
 
-    Response = {}
+     //==========================================================================
+     // GLOBAL VARIABLES
 
-    constructor() {
-        super();
+     //==========================================================================
+     // CONSTRUCTOR
 
-        // init the enum int type to identify the type of the response of the connectSocketIO function.
-        this.Response = {
-            INPUT_ERROR: 0,          // some data is missing or corrupted in the input
-            BRIDGE_ACCESS_ERROR: 1,  // bridge is destroyed or bad instantiation
-            UNKNOWN_ERROR: 2,        // no ideia
-            SERVICE_STARTED: 3       // Service started with success, NOT known if the connection was a success.
-        };
-        // cannot alter the object values of the AudioOutputRoute
-        Object.freeze(this.Response);
-    }
+     constructor() {
+         super();
 
-    //==========================================================================
-    // METHODS
+         this.requestAuthorization = this.requestAuthorization.bind(this);
+         this.requestDeviceToken = this.requestDeviceToken.bind(this);
+         this.requestCallStatus = this.requestCallStatus.bind(this);
+         this.getCallData = this.getCallData.bind(this);
+     }
 
-    /**
-     *
-     * @async
-     * @return {}
-     */
-    async test() : string {
-        console.log("CallManager test not exist for Android");
-        return "";
-    }
+     //==========================================================================
+     // METHODS
 
-    /**
-     *
-     * @async
-     * @return {}
-     */
-    async connectSocketIO(ipAddress : String, mainBundlePackageName : String, serverChannel : String) : int {
-        return await NativeModules.CallManagerModule.connectSocketIO(ipAddress, mainBundlePackageName, serverChannel);
-    }
-}
+     async requestAuthorization() {
+         console.log("Not implemented in Android, see OneSignal documentation.");
+     }
 
-//==========================================================================
-// EXPORTS
+     async requestDeviceToken() {
+         console.log("Not implemented in Android, see OneSignal documentation.");
+     }
 
-module.exports = new CallManager();
+     async requestCallStatus() {
+         console.log("Not implemented in Android, see OneSignal documentation.");
+     }
+
+     async getCallData() {
+         console.log("Not implemented in Android, see OneSignal documentation.");
+     }
+ }
+
+ //==========================================================================
+ // EXPORTS
+
+ module.exports = new CallManager();
