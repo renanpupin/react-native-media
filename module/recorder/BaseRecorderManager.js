371b798a1c2281620576a2346525ca5f9ddbd9ce
HaroldoTeruya <Haroldo_Teruya@hotmail.com>
2018-04-18 08:32:32 -0300

    Ready to start development. Configuration OK

---
+++ module/recorder/BaseRecorderManager.js
@@ -0,0 +1,37 @@
+/**
+ * @author Haroldo Shigueaki Teruya <haroldo.s.teruya@gmail.com>
+ * @version 1.0
+ */
+
+//==========================================================================
+// IMPORTS
+
+/**
+ * This class requires:
+ * @class
+ * @requires NativeModules from react-native
+ */
+import { NativeModules } from 'react-native';
+
+//==========================================================================
+
+/**
+ * @class
+ * @classdesc
+ */
+class BaseRecorderManager {
+
+    //==========================================================================
+    // GLOBAL VARIABLES
+
+    //==========================================================================
+    // CONSTRUCTOR
+
+    //==========================================================================
+    // METHODS
+
+    //==========================================================================
+    // SETTERS & GETTERS
+}
+
+module.exports = BaseRecorderManager;
---
+++ module/recorder/index.android.js
@@ -0,0 +1,33 @@
+/**
+ * @author Haroldo Shigueaki Teruya <haroldo.s.teruya@gmail.com>
+ * @version 1.0
+ */
+
+//==========================================================================
+// IMPORTS
+
+/**
+ * This class requires:
+ * @class
+ * @requires [BaseRecorderManager]{@link ./BaseRecorderManager}
+ * @requires NativeModules from react-native
+ */
+import BaseRecorderManager from './BaseRecorderManager';
+import { NativeModules } from 'react-native';
+
+//==========================================================================
+
+/**
+ * @class
+ * @classdesc
+ */
+class RecorderManager extends BaseRecorderManager {
+
+    //==========================================================================
+    // METHODS
+}
+
+//==========================================================================
+// EXPORTS
+
+module.exports = new RecorderManager();
